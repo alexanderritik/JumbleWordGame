cb74fe17d380dc191b30c04adf124f67e5d622f7
Ritik <rakeshkusrivastav@gmail.com>
2020-08-26 23:58:33 +0530

    updated UI

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -12,11 +12,18 @@ class ViewController: UITableViewController {
     var allWord = [String]()
     var playerWord = [String]()
     
+    @IBOutlet var image: UIImageView!
+    var img = #imageLiteral(resourceName: "yellow")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: #selector(promptWord))
+        title = "JUMBLE GAME"
+        
+        image.layer.cornerRadius = 50
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptWord))
         
         if let allWordUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWord = try? String(contentsOf: allWordUrl){
@@ -33,9 +40,12 @@ class ViewController: UITableViewController {
     
     
     func startGame(){
-        title = allWord.randomElement()
+        let heading = allWord.randomElement()
         playerWord.removeAll(keepingCapacity: true)
         tableView.reloadData()
+        
+        let img1 = textToImage(drawText: heading!, inImage: img, atPoint: CGPoint(x: 150, y: 220))
+        image.image = img1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -50,20 +60,69 @@ class ViewController: UITableViewController {
     
     
     @objc func promptWord(){
-        let ac = UIAlertController(title: "Enter Answere", message: nil, preferredStyle: .alert)
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
         
-        let submitAction = UIAlertAction(title: "Submit", style: .default){
-            [weak self ,weak ac] action in
-            guard let answer = ac?.textFields?[0].text else{ return }
-            self.submit(answer)
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer:String){
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer){
+            if isReal(word: lowerAnswer){
+                if isOriginal(word: lowerAnswer){
+                    playerWord.insert(lowerAnswer, at: 0)
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
         }
         
-        present(ac,animated: true)
+    }
+    
+    func isPossible(word:String)-> Bool {
+        return true;
+    }
+    
+    func isOriginal(word:String)-> Bool {
+        return !allWord.contains(word);
+    }
+    
+    func isReal(word:String)-> Bool {
+        return true;
     }
     
     
-    
+    func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
+        
+        let textColor = UIColor.black
+        let textFont = UIFont(name: "Helvetica Bold", size: 160)!
+
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
+
+        let textFontAttributes = [
+            NSAttributedString.Key.font: textFont,
+            NSAttributedString.Key.foregroundColor: textColor,
+            ] as [NSAttributedString.Key : Any]
+        
+        image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
+
+        let rect = CGRect(origin: point, size: image.size)
+        text.draw(in: rect, withAttributes: textFontAttributes)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
+    }
 }
 
  
