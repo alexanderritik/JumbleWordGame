c7d89ff7429e56e326aa9154637fbb50b8fdf050
Ritik <rakeshkusrivastav@gmail.com>
2020-08-26 19:34:58 +0530

    modified storyboard point

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: #selector(promptWord))
+        
         if let allWordUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWord = try? String(contentsOf: allWordUrl){
                 allWord = startWord.components(separatedBy: "\n");
@@ -25,17 +27,32 @@ class ViewController: UITableViewController {
         if allWord.isEmpty {
             allWord = ["silkWorm"]
         }
-        
+        startGame()
+    
     }
     
     
     func startGame(){
         title = allWord.randomElement()
+        print(title)
         playerWord.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return playerWord.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word",for: indexPath)
+        cell.textLabel?.text = playerWord[indexPath.row]
+        return cell
+    }
+    
+    
+    @objc func promptWord(){
+        
+    }
 }
 
  
